3bd466e06fd231ec957071bb554fdac73e8f2ef7
Dmitry <badl1970@mail.ru>
2022-05-28 23:54:32 +0300

    Homework is done.

---
+++ src/main/java/ru/gb/baranov/Main.java
@@ -0,0 +1,9 @@
+package ru.gb.baranov;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+
+}
---
+++ src/main/java/ru/gb/baranov/homework1/Barrier.java
@@ -0,0 +1,7 @@
+package ru.gb.baranov.homework1;
+
+public interface Barrier {
+
+    boolean isObstacle(Participant p);
+
+}
